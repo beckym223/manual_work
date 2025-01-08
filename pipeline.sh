8bf68f783e8f93b0a055f85e9d5bfd870c7f24df
beckym223 <marcu22r@mtholyoke.edu>
2025-01-07 18:38:00 -0800

    bash

---
+++ pipeline.sh
@@ -0,0 +1,7 @@
+CURRENT_BRANCH=$(git branch --show-current)
+
+# Generate a unique branch name
+BRANCH_NAME="pipeline-run-$(date +'%Y%m%d%H%M%S')"
+
+# Create and switch to the new branch, setting the upstream to the current branch
+git checkout -b "$BRANCH_NAME" --track "$CURRENT_BRANCH"

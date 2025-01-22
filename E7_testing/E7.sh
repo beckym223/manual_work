5daf0d41918ca8f492b0cd3d31303b472cddc400
beckym223 <marcu22r@mtholyoke.edu>
2025-01-22 11:47:40 -0800

    renamed

--- E7_testing/E7.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Directories and log file for E2
-SOURCE_DIR="/Users/BeckyMarcusMacbook/Thesis/manual_work/E7_testing/raw_texts"
-DEST_DIR="/Users/BeckyMarcusMacbook/Thesis/manual_work/E7_testing/temp_clean"
-
-# Call the common script
-python /Users/BeckyMarcusMacbook/Thesis/manual_work/E7_testing/E7.py "$SOURCE_DIR" "$DEST_DIR" 
